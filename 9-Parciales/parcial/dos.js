42aa71a483041c04e1d66e198cc798ce64bca82d
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 11:53:43 -0300

    importefinal

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,14 +1,12 @@
 function Mostrar()
-{
-  
-  
+{ 
   var importe;
   importe = prompt("");
   importe = parseInt(importe);
   var importeFinal;
   var iva;
-  iva = 21%;
-  iva = parseInt(21%);
+  iva = 21;
+  iva = parseInt(21);
   importeFinal = document.getElementById("ImporteFinal").value;
   importeFinal = importe + iva;
 

1980153e8d091acce9cd55e84d2123a091131807
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 12:19:49 -0300

    triangulo

--- 9-Parciales/parcial2/1-entradaSalida.js
+++ 9-Parciales/parcial2/1-entradaSalida.js
@@ -1,7 +1,7 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 1-EntradaSalida");
+	
 	var lado;
 	lado = document.getElementById("lado").value;
 	perimetro = lado * 3;

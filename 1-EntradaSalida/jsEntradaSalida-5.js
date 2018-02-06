7e31595368a0139132d597981c2e83f3de962d23
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 09:16:41 -0300

    js5

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -8,6 +8,6 @@ function Mostar()
     nombre = document.getElementById('elNombre').value;
     edad = document.getElementById('laEdad').value;
 
-    alert("Usted se llama" + nombre + "y tiene" + edad + "años")
+    alert("Usted se llama" + nombre + " y tiene " + edad + " años")
 }
 

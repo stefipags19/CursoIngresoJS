4b9d8c14a06942ebf79e3fbeacc96e4f2efa1893
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 11:21:53 -0300

    perimetro

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,11 +1,11 @@
 
 function Mostrar()
 {
-    var cuadrado;
+    
     var base;
     base = document.getElementById("laBase").valor;
     perimetro = base * 4;
-    alert("El perimetro de un cuadrado es " perimetro);
+    alert("El perimetro de un cuadrado es " + perimetro);
 
     
 

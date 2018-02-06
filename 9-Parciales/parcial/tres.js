a71d210cf3bd5dbd02c154a6e88f2ee7a467831a
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 12:03:21 -0300

    perimetro2

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -2,7 +2,7 @@ function Mostrar()
 {
     var largo;
     var ancho;
-    var perimetro;
+    
 
     largo = document.getElementById("alrgo").value;
     ancho = document.getElementById("ancho").value;

726c1191b42d5fc92d3695f7f756d9b53520efeb
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 12:02:02 -0300

    perimetro

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,14 @@
 function Mostrar()
 {
+    var largo;
+    var ancho;
+    var perimetro;
 
+    largo = document.getElementById("alrgo").value;
+    ancho = document.getElementById("ancho").value;
+
+    perimetro = (largo * ancho) * 3;
+    perimetro = parseInt(perimetro);
+
+    alert("Se necesitan " perimetro " metros");
 }

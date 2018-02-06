c13080ce62af5a6dccd07b8721b7994901f000f5
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 09:47:25 -0300

    js6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -6,12 +6,14 @@ function sumar()
     var num1;
     var num2;
     var suma;
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    suma = num1 + num2;
+    
 
     numeroUno = document.getElementById("numeroUno").value;
     numeroDos = document.getElementById("numeroDos").value;
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    suma = num1 + num2;
 
     alert("La suma es " + suma);
 }

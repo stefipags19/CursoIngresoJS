8207c168775dba73c6f59f73e5894d2fd46c5649
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 22:31:21 -0300

    importefinal2

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -7,11 +7,11 @@ function Mostrar()
 	
 	
 	importe = prompt("");
-	
-	importe = document.getElementById("importe").value;
 	importe = parseInt(importe);
+	importe = document.getElementById("importe").value;
+	
 	importeFinal = importe * 1.21;
-	importeFinal = parseInt(importeFinal);
+	
 
 	
 

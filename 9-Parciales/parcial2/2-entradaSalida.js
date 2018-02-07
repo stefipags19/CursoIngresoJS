4d05608d53c9893aa2941c0919a1df0840457c64
stefipags19 <stefi.pagnini@gmail.com>
2018-02-06 22:13:00 -0300

    importefinal2

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -3,13 +3,13 @@ function Mostrar()
 {
 	alert("Funciona 2-EntradaSalida");
 	var importe;
-	var importeFinal;
-	var iva;
-	iva = 1.21;
+	
+	
 	importe = prompt("");
 	importe = parseInt(importe);
 	importe = document.getElementById(importe).value;
 	importeFinal = importe * 1.21;
+
 	
 
 	alert("El importe final es " + importeFinal);

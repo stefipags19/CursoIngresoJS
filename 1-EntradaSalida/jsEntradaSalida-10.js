1cf29b41cd4fba15cf3a0a4fccc5f2e561599655
stefipags19 <stefi.pagnini@gmail.com>
2018-02-11 01:57:58 -0300

    js10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,9 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	var = precioTotal;
-    var = importe;
-    var = descuento;
-    
-    precioTotal = importe - descuento;
+    var importe;
+    var resultado;
+    var descuento;
+
+    importe = document.getElementById("importe").value;
+    importe = parseInt(importe);
+    descuento = (25 * importe) / 100;
+
+    resultado = importe - descuento;
+    document.getElementById("resultado").value = resultado;
 }

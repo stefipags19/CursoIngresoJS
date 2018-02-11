a8b79095f72a9d1a3d8aebc7b5658e434bb88c93
stefipags19 <stefi.pagnini@gmail.com>
2018-02-11 00:53:45 -0300

    terreno

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -2,6 +2,17 @@
 function Mostrar()
 {
 	alert("Funciona 3-EntradaSalida");
+
+	var largo;
+	var ancho;
+
+	largo = document.getElementById("largo").value;
+	ancho = document.getElementById("ancho").value;
+
+	perimetro = (largo * 2 + ancho * 2) * 3;
+	perimetro = parseInt(perimetro);
+	
+	alert("Se necesitan " + perimetro + " metros");
 	
 }
 

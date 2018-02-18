f2d6b1af1b748dd7fa8700923f0f4afce504c258
stefipags19 <stefi.pagnini@gmail.com>
2018-02-18 01:31:13 -0300

    random

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,7 +1,11 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	Math.floor(Math.random() * 11);
+	var random;
+	random = Math.floor(Math.random() * 11) + 1;
+
+	alert(random);
+	
 	
 
 }//FIN DE LA FUNCIÓN

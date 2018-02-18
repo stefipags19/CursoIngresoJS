ea8377fb3312a22ea7530d14f556fea50d1ded82
stefipags19 <stefi.pagnini@gmail.com>
2018-02-17 23:31:41 -0300

    adolescente

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+if(edad >=13<=17) {
+    alert("Es adolescente");
+}
 
 
 }//FIN DE LA FUNCIÓN

046c427305e123dd97c7a51c99f2cbb3da1299d8
stefipags19 <stefi.pagnini@gmail.com>
2018-02-18 00:05:00 -0300

    edad

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+if(edad >= 18) {
+    alert("Es mayor de edad");
+}
+else if(edad >= 13 && edad <= 17) {
+    alert("Es adolescente");
+}
+else {
+    alert("Es un niño");
+}
 
 
 

dbcbbae5f8015cbf85756d82c30ec052e2da4b66
stefipags19 <stefi.pagnini@gmail.com>
2018-02-17 23:45:07 -0300

    noadolescente

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+if(edad <= 13 && edad >= 17) {
+    alert("No es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN

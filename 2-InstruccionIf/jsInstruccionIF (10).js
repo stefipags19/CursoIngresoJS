a78b3c75615f06ba62f5a94f6cb42aa69eab967d
stefipags19 <stefi.pagnini@gmail.com>
2018-02-18 01:36:49 -0300

    notas

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var random;
+	random = Math.floor(Math.random() * 11) + 1;
+
+	if(random >= 9) {
+		alert("Excelente");
+	}
+	else if(random >= 4) {
+		alert("Aprobó");
+	}
+	else {
+		alert("Vamos, la proxima se puede");
+	}
 	
 
 }//FIN DE LA FUNCIÓN

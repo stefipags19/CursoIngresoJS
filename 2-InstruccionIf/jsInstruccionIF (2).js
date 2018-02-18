522eb7af373bdb718e63f4744e0fe8b2509e3047
stefipags19 <stefi.pagnini@gmail.com>
2018-02-17 23:24:20 -0300

    mayordeedad

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -6,11 +6,12 @@ var edad
 edad = document.getElementById("edad").value;
 edad = parseInt(edad);
 
-if(edad => 18) {
+if(edad >= 18) {
     alert("Es mayor de edad");
 }
 else {
     alert("No es mayor de edad");
+
 }
 
 

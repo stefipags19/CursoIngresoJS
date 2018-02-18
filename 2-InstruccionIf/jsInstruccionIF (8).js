5a553a7ecde47806b5690e9fa6a00356fe439045
stefipags19 <stefi.pagnini@gmail.com>
2018-02-18 00:33:56 -0300

    estadociviledad

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
+edad = document.getElementById(edad).value;
+edad = parseInt(edad);
+
+if(edad < 18 && estadoCivil == "Soltero"){
+    alert("Es soltero y no es menor");
+}
 	
 
 

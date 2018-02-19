f6388506fef7500f0dc57e713aab13cc03bc013a
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 20:53:55 -0300

    mes312830

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -7,6 +7,12 @@ var mesDelAño = document.getElementById('mes').value;
 switch(mesDelAño)
 {
     case "Enero" :
+    case "Marzo" :
+    case "Mayo" :
+    case "Julio" :
+    case "Agosto" :
+    case "Octubre" :
+    case "Diciembre" :
     {
         alert("Este mes tiene 31 dias");
         break;
@@ -16,54 +22,12 @@ switch(mesDelAño)
         alert("Este mes tiene 28 dias");
         break;
     }
-    case "Marzo" :
-    {
-        alert("Este mes tiene 31 dias");
-        break;
-    }
     case "Abril" :
-    {
-        alert("Este mes tiene 30 dias");
-        break;
-    }
-    case "Mayo" :
-    {
-        alert("Este mes tiene 31 dias");
-        break;
-    }
     case "Junio" :
-    {
-        alert("Este mes tiene 30 dias");
-        break;
-    }
-    case "Julio" :
-    {
-        alert("Este mes tiene 31 dias");
-        break;
-    }
-    case "Agosto" :
-    {
-        alert("Este mes tiene 31 dias");
-        break;
-    }
     case "Septiembre" :
-    {
-        alert("Este mes tiene 30 dias");
-        break;
-    }
-    case "Octubre" :
-    {
-        alert("Este mes tiene 31 dias");
-        break;
-    }
     case "Noviembre" :
     {
         alert("Este mes tiene 30 dias");
-        break;
-    }
-    case "Diciembre" :
-    {
-        alert("Este mes tiene 31 dias");
         break;
     }
 

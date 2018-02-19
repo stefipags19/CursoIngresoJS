2c6061f101c97cabe6c0fe0a8005741325a9a41d
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 12:32:24 -0300

    inviernomes

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -6,17 +6,63 @@ var mesDelAño = document.getElementById('mes').value;
 
 switch(mesDelaño)
 {
-    case "Julio", "Agosto" :
+    case "Julio" :
     {
         alert("Abrigate que hace frio.");
         break;
     }
-    case "Septiembre", "Octubre", "Noviembre", "Diciembre", "Enero" :
+    case "Agosto" :
+    {
+        alert("Abrigate que hace frio.");
+        break;
+    }
+    case "Septiembre" :
     {
         alert("Ya pasamos el frio, ahora calor!");
         break;
     }
-    case "Febrero", "Marzo",  "Abril", "Mayo", "Junio" :
+    case "Octubre" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Noviembre" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "December" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Enero":
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+
+    case "Febrero": "Marzo",  "Abril", "Mayo", "Junio" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Marzo" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Abril" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Mayo" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Junio" :
     {
         alert("Falta para el invierno");
         break;

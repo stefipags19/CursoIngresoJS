990c1df48844808af8347ef1634e9c20bab7a601
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 13:12:17 -0300

    treintayuno

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,73 @@
 function Mostrar()
 {
 //tomo la edad  
+var mesDelAño;
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Enero" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+    case "Febrero" :
+    {
+        alert("Este mes tiene 28 dias");
+        break;
+    }
+    case "Marzo" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+    case "Abril" :
+    {
+        alert("Este mes tiene 30 dias");
+        break;
+    }
+    case "Mayo" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+    case "Junio" :
+    {
+        alert("Este mes tiene 30 dias");
+        break;
+    }
+    case "Julio" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+    case "Agosto" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+    case "Septiembre" :
+    {
+        alert("Este mes tiene 30 dias");
+        break;
+    }
+    case "Octubre" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+    case "Noviembre" :
+    {
+        alert("Este mes tiene 30 dias");
+        break;
+    }
+    case "Diciembre" :
+    {
+        alert("Este mes tiene 31 dias");
+        break;
+    }
+
+}
 	
 	
 

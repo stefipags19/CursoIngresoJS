97823d78d63c1537e244cbd6679e1d137dd93bbf
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 12:53:24 -0300

    mesinvierno

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -4,15 +4,71 @@ function Mostrar()
 var mesDelAño; 
 var mesDelAño = document.getElementById('mes').value;
 
-if(mesDelAño === "Marzo", "Abril", "Mayo", "Junio") {
-    alert("Falta para el invierno.");
-}
-else if(mesdelAño === "Julio", "Agosto") {
-    alert("Abrigate que hace frio.");
-}
-else
+
+switch(mesDelAño)
 {
-    alert("Ya pasamos el frio, ahora calor!");
+    case "Julio" :
+    {
+        alert("Abrigate que hace frio.");
+        break;
+    }
+    case "Agosto" :
+    {
+        alert("Abrigate que hace frio.");
+        break;
+    }
+    case "Septiembre" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Octubre" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Noviembre" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "December" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Enero":
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Febrero" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Marzo" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Abril" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Mayo" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Junio" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
 }
+
+
 
 }//FIN DE LA FUNCIÓN

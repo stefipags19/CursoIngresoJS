51ab69372acea51fb8a438d47d438006aea2d9a6
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 13:49:01 -0300

    mesinvierno

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -8,60 +8,24 @@ var mesDelAño = document.getElementById('mes').value;
 switch(mesDelAño)
 {
     case "Julio" :
-    {
-        alert("Abrigate que hace frio.");
-        break;
-    }
     case "Agosto" :
     {
         alert("Abrigate que hace frio.");
         break;
     }
     case "Septiembre" :
-    {
-        alert("Ya pasamos el frio, ahora calor!");
-        break;
-    }
     case "Octubre" :
-    {
-        alert("Ya pasamos el frio, ahora calor!");
-        break;
-    }
     case "Noviembre" :
-    {
-        alert("Ya pasamos el frio, ahora calor!");
-        break;
-    }
-    case "Deiciembre" :
-    {
-        alert("Ya pasamos el frio, ahora calor!");
-        break;
-    }
+    case "Diciembre" :
     case "Enero":
     {
         alert("Ya pasamos el frio, ahora calor!");
         break;
     }
     case "Febrero" :
-    {
-        alert("Falta para el invierno");
-        break;
-    }
     case "Marzo" :
-    {
-        alert("Falta para el invierno");
-        break;
-    }
     case "Abril" :
-    {
-        alert("Falta para el invierno");
-        break;
-    }
     case "Mayo" :
-    {
-        alert("Falta para el invierno");
-        break;
-    }
     case "Junio" :
     {
         alert("Falta para el invierno");

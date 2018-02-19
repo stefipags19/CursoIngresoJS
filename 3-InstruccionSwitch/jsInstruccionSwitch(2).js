772b4429e98158140e98e25208c210547db92995
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 12:05:45 -0300

    invierno

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,9 +2,26 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
 alert (mesDelAño);
 
+switch(mesDelaño)
+{
+    case "Julio" || "Agosto" :
+    {
+        alert("Abrigate que hace frio.");
+        break;
+    }
+    case "Septiembre" || "Octubre"|| "Noviembre"|| "Diciembre" || "Enero" :
+    {
+        alert("Ya pasamos el frio, ahora calor!");
+        break;
+    }
+    case "Febrero" || "Marzo" || "Abril" || "Mayo" || "Junio" :
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+}
 
 
 

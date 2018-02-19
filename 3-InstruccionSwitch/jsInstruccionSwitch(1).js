84d52c256cb43fdc37d671a759dd24e722b5b2bc
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 11:44:06 -0300

    mesdelaño

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,29 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{
+    case Enero:
+    {
+        console.log("¡Que comiences bien el año!.");
+        break;
+    }
+    case Marzo:
+    {
+        console.log("¡A clases!.");
+        break;
+    }
+    case Julio:
+    {
+        console.log("¡Se vienen las vacaciones!.");
+        break;
+    }
+    case Diciembre:
+    {
+        console.log("¡Felices fiestas!.");
+        break;
+    }
+}
 
 
 

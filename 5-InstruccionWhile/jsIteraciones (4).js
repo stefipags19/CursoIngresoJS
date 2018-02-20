c2a5af0a6ee24722a45e1c4b4d460459d2c5918f
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 21:59:36 -0300

    validar1al10

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,10 @@ function Mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
+	while(numero >= 1 && numero <= 10) {
+		alert("Numero validado");
+		break;
+	}
+
 
 }//FIN DE LA FUNCIÓN

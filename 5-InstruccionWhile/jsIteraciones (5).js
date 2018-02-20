a342e4c61fc48050fff7e8f45d8dfcbc3a673d91
stefipags19 <stefi.pagnini@gmail.com>
2018-02-20 00:29:30 -0300

    fom

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,10 +2,13 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
-document.getElementById('Sexo').value=sexo;
 
-while(sexo != f || sexo != m) {
+while(sexo != "f" || sexo != "m") {
     sexo = prompt("ingrese f ó m .");
 }
+
+document.getElementById('Sexo').value=sexo;
+
+
 
 }//FIN DE LA FUNCIÓN

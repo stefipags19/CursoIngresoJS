4a54dd408c0435155ef968c7907187eb6617eecb
stefipags19 <stefi.pagnini@gmail.com>
2018-02-20 00:09:23 -0300

    numero1a10

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -4,9 +4,12 @@ function Mostrar()
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
 	while(numero >= 1 && numero <= 10) {
-		alert("Numero validado");
-		break;
+		numero = prompt("Valor incorrecto, por favor ingrese un número entre 0 y 10.");
 	}
+
+	alert("¡Bien! Ingresaste el numero " +  numero);
+
+
 
 
 }//FIN DE LA FUNCIÓN

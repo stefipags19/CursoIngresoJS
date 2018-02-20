fd29045b4e6289cbfaf4de6159eddb56dc27a046
stefipags19 <stefi.pagnini@gmail.com>
2018-02-20 00:13:08 -0300

    numbervalid

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,7 +3,7 @@ function Mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
-	while(numero < 1 || theNumber > 10) {
+	while(numero < 1 || numero > 10) {
 		numero = prompt("Valor incorrecto, por favor ingrese un número entre 0 y 10.");
 	}
 

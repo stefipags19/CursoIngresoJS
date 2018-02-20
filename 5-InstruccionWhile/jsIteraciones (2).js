44ada1aea9913c2334c6721952df0f9a79c14d61
stefipags19 <stefi.pagnini@gmail.com>
2018-02-19 21:30:20 -0300

    contadordescendiente

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -2,5 +2,13 @@ function Mostrar()
 {
 	alert('iteración while');
 
+	var contador;
+	contador = 1;
+
+	while(contador <= 10) {
+		alert("Numero : " + contador);
+		contador = contador - 1;
+	}
+
 
 }//FIN DE LA FUNCIÓN

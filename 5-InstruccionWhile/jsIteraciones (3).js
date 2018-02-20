e6c868d7f744aa3c8cbf1e4be629199dcdf1a454
stefipags19 <stefi.pagnini@gmail.com>
2018-02-20 00:16:19 -0300

    utn750

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,14 +1,14 @@
 function Mostrar()
 {
 
-var clave = prompt("Ingrese el número clave.");
+    var clave = prompt("Ingrese el número clave.");
 
 
-while(clave != "utn750") {
-    prompt("Ingrese el numero clave nuevamente.");
-    clave = "utn750";
+    while(clave != "utn750") {
+    clave = prompt("Ingrese el numero clave nuevamente.");
+    }
 
-}
+    alert("Numero clave correcto.")
 
 
 }//FIN DE LA FUNCIÓN

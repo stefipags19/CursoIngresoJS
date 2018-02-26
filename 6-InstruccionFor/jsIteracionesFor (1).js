b0cf2b0d2368c620bf9cfb5435406ba137155465
stefipags19 <stefi.pagnin@gmail.comi>
2018-02-26 08:45:00 -0300

    contador1

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -3,7 +3,7 @@ function Mostrar()
     var contador;
 
     for(contador = 1; contador < 11; contador++) {
-        console.log("Numero :" + contador);
+        alert("Numero :" + contador);
     }
 
 }

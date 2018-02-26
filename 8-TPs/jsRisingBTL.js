3009bca8e1f71f7604c35fcdae79289261a43f30
stefipags19 <stefi.pagnin@gmail.comi>
2018-02-26 09:30:34 -0300

    risingbtl

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -19,30 +19,40 @@ function ComenzarIngreso ()
     while(edad < 18 && edad > 90 ){
         edad = prompt(" Por favor ingrese un valor entre 18 y 90 años inclusive.");
     }
-    edad = document.getElementById("Edad").value;
+    
 
     while(sexo != "f" && sexo != "m"){
         sexo = prompt(" Por favor ingrese f ó m.");
     }
-    sexo = document.getElementById("Sexo").value;
+    
 
     while(estadoCivil != 1 && estadoCivil != 2 && estadoCivil !=3 && estadoCivil != 4){
         estadoCivil = prompt(" Por favor ingrese 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
     }
-    estadoCivil = document.getElementById("EstadoCivil").value;
-
+    
     while(sueldoBruto < 8000) {
         sueldoBruto = prompt(" Por favor ingrese un valor no menor a 8000");
     }
-    sueldoBruto = document.getElementById("Sueldo").value;
+    
 
     while(numeroLegajo < 1000 && numeroLegajo > 10000) {
         numeroLegajo = prompt("Por favor ingrese un valor numérico de 4 cifras, sin ceros a la izquierda.");
     }
-    numeroLegajo = document.getElementById("Legajo").value;
+    
 
     while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N"){
         nacionalidad = prompt("Por favor ingrese el valor “A” para argentinos, “E” para extranjeros, “N” para nacionalizados");
     }
+    
  
+
+     edad = document.getElementById("Edad").value;
+     sexo = document.getElementById("Sexo").value;
+     estadoCivil = document.getElementById("EstadoCivil").value;
+     sueldoBruto = document.getElementById("Sueldo").value;
+     numeroLegajo = document.getElementById("Legajo").value;
+     nacionalidad = document.getElementById("Nacionalidad").value;
+
+
+
 }
